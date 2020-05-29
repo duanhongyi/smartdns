f3f66b9ef371d7aec07f386928b7e184d4f277d5
duanhongyi <duanhongyi@doopai.com>
2020-05-29 17:53:42 +0800

    chore: update version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
 
     name='smartdns',
     description='An Intelligent DNS Similar to DNSPod',
-    version='3.2.1',
+    version='3.2.2',
     author='duanhongyi',
     author_email='duanhyi@gmail.com',
     packages=find_packages(),
